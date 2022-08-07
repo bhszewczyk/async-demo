685750bde022734d5dd3ae76842cf467f64ae9dd
Barbara Szewczyk <comamysza@gmail.com>
2022-08-07 20:01:41 +0200

    Adds callback hell - nested callbacks

--- callback.js
+++ callback.js
@@ -8,3 +8,32 @@ const fakeRequestCallback = (url, success, failure) => {
 		}
 	}, delay);
 };
+
+// callback hell
+fakeRequestCallback(
+	'books.com/page1',
+	function () {
+		console.log('IT WORKED!');
+		fakeRequestCallback(
+			'books/com/page2',
+			function () {
+				console.log('IT WORKED FOR THE SECOND TIME');
+				fakeRequestCallback(
+					'books.com/page3',
+					function () {
+						console.log('IT WORKED FOR THE THIRD TIME');
+					},
+					function () {
+						console.log('PAGE 3 TIMEOUT');
+					}
+				);
+			},
+			function () {
+				console.log('PAGE 2 TIMEOUT');
+			}
+		);
+	},
+	function () {
+		console.log('TIMEOUT :(');
+	}
+);
