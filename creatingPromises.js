fc25bc247ddcf1d05d3bb6f4daea978ccbcf1c48
Barbara Szewczyk <comamysza@gmail.com>
2022-08-07 21:11:02 +0200

    Adds example with background color change

--- creatingPromises.js
+++ creatingPromises.js
@@ -16,3 +16,17 @@ fakeRequest('udemy.com')
 		console.log('data received:', data);
 	})
 	.catch((err) => console.log('udemy timeout', err));
+
+const delayedColorChange = (color, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			document.body.style.backgroundColor = color;
+			resolve();
+		}, delay);
+	});
+};
+
+delayedColorChange('red', 1000)
+	.then(() => delayedColorChange('green', 1000))
+	.then(() => delayedColorChange('blue', 1000))
+	.then(() => delayedColorChange('cyan', 1000));
