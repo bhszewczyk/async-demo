5f7c4d7b0a575ca96c1c48df34fe6fc54a809010
Barbara Szewczyk <comamysza@gmail.com>
2022-08-07 20:13:01 +0200

    Adds fakeRequestPromise

---
+++ promises.js
@@ -0,0 +1,12 @@
+const fakeRequestPromise = (url) => {
+	return new Promise((resolve, reject) => {
+		const delay = Math.floor(Math.random() * 4500) + 500;
+		setTimeout(() => {
+			if (delay > 4000) {
+				reject('Connection Timeout :(');
+			} else {
+				resolve(`Here is your fake data from ${url}`);
+			}
+		}, delay);
+	});
+};
