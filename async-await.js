ee7156f040fab06c33c6caf8943d56c192dd526c
Barbara Szewczyk <comamysza@gmail.com>
2022-08-07 21:57:59 +0200

    Adds try catch for async-await rejection

--- async-await.js
+++ async-await.js
@@ -19,3 +19,13 @@ login('whatever', 'corgi')
 		console.log('Login failed');
 		console.log(err);
 	});
+
+//how we can handle rejected promises in async await functions
+async function retreiveDataOnLogin() {
+	try {
+		let data1 = await login('username', 'secret');
+		console.log(data1);
+	} catch (e) {
+		console.log('error', e);
+	}
+}
