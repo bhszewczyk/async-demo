5b413510ae6ff7eb31c3b4546a7a43f3421bb69e
Barbara Szewczyk <comamysza@gmail.com>
2022-08-08 21:48:31 +0200

    Changes axios request to be async await function

--- axios.js
+++ axios.js
@@ -1,8 +1,15 @@
-const getStarWarsStarships = async () => {
-	const response = await axios
-		.get('https://swapi.dev/api/starships/9/')
-		.then((res) => console.log(res.data))
-		.catch((err) => console.log('Error:', err.message));
+const getStarWarsStarships = async (id) => {
+	try {
+		const response = await axios
+			.get(`https://swapi.dev/api/starships/${id}/`)
+			.then((res) => console.log(res.data));
+	} catch (err) {
+		console.log('Error:', err.message);
+	}
 };
 
-getStarWarsStarships();
+const id = Math.floor(Math.random() * 10);
+getStarWarsStarships(id);
+getStarWarsStarships(4);
+getStarWarsStarships(5);
+getStarWarsStarships(6);
