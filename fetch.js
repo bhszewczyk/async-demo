cf1d0bcb9627b227fa022d211c9c8be3f5d0d39f
Barbara Szewczyk <comamysza@gmail.com>
2022-08-08 21:14:05 +0200

    Adds async fetch with try and catch

--- fetch.js
+++ fetch.js
@@ -1,11 +1,23 @@
-fetch('https://swapi.dev/api/people/1')
-	.then((res) => {
-		console.log(res);
-		return res.json();
-	})
-	.then((data) => {
+// fetch('https://swapi.dev/api/people/1')
+// 	.then((res) => {
+// 		console.log(res);
+// 		return res.json();
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
+
+const loadStarWarsPeople = async () => {
+	try {
+		const response = await fetch('https://swapi.dev/api/people/1');
+		const data = await response.json();
 		console.log(data);
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+	} catch (err) {
+		console.log('Something went wrong :(', err);
+	}
+};
+
+loadStarWarsPeople();
